3188039f6b1351a7bd51ddd62edd6e2da37ea6cd
GuillaumeDamour1 <guillaumedocitedamour@gmail.com>
2022-12-02 11:31:05 +0100

    ajout admissibilité d'un triangle, critère de Delaunay

--- main.cpp
+++ main.cpp
@@ -20,32 +20,6 @@ struct maillage
 };
 
 
-double distance2D(double x1, double y1, double x2, double y2)
-{
-	double dist=0.0;
-
-	dist=sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-
-	return dist;
-}
-
-double Qualite2D(int i, struct maillage mesh)
-{
-	double Q, aire, s1x, s1y, s2x, s2y, s3x, s3y;
-	s1x = mesh.Vertices[(mesh.Triangles[3*i]-1)*2];
-	s1y = mesh.Vertices[(mesh.Triangles[3*i]-1)*2+1];
-	s2x = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2];
-	s2y = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2+1];
-	s3x = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2];
-	s3y = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2+1];
-
-	aire = abs((1./2.)*((s2x-s1x)*(s3y-s1y)-(s3x-s1x)*(s2y-s1y)));
-
-	Q = (sqrt(3.0)/(12.0*aire))*(distance2D(s1x, s1y, s2x, s2y)+distance2D(s1x, s1y, s3x, s3y)+distance2D(s3x, s3y, s2x, s2y));
-
-	return Q;
-}
-
 int main(int argc, char* argv[])
 {
 	ifstream monFlux("/home/guillaume/Bureau/Enseirb_Matmeca/CHP2/Technique/TP1/meshes/2D/square_disc.mesh");
@@ -129,5 +103,79 @@ int main(int argc, char* argv[])
 		}
 	}
 
+	//On est sur le triangle contenant le point initial donc il est forcement noté -1
+	for (arretes du triangle contenant le point noté -1)
+		Tant qu'on ne tombe pas sur un triangle ayant deux voisins noté 0 on continue à aller sur ses triangles voisins
+			fonction(sur nouveau triangle)
+			Tant qu'on ne tombe pas sur un triangle ayant deux voisins noté 0 on continue à aller sur ses triangles voisins
+				fonction(sur nouveau triangle)
+
+
 	return 0;
+}
+
+double distance2D(double x1, double y1, double x2, double y2)
+{
+	double dist=0.0;
+
+	dist=sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+
+	return dist;
+}
+
+double Qualite2D(int i, struct maillage mesh)
+{
+	double Q, aire, s1x, s1y, s2x, s2y, s3x, s3y;
+	s1x = mesh.Vertices[(mesh.Triangles[3*i]-1)*2];
+	s1y = mesh.Vertices[(mesh.Triangles[3*i]-1)*2+1];
+	s2x = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2];
+	s2y = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2+1];
+	s3x = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2];
+	s3y = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2+1];
+
+	aire = abs((1./2.)*((s2x-s1x)*(s3y-s1y)-(s3x-s1x)*(s2y-s1y)));
+
+	Q = (sqrt(3.0)/(12.0*aire))*(distance2D(s1x, s1y, s2x, s2y)+distance2D(s1x, s1y, s3x, s3y)+distance2D(s3x, s3y, s2x, s2y));
+
+	return Q;
+}
+
+int admissibilite(double x, double y, int i, struct maillage mesh)
+{
+	//Sommets du triangle i
+	double s1x, s1y, s2x, s2y;
+	s1x = mesh.Vertices[(mesh.Triangles[3*i]-1)*2];
+	s1y = mesh.Vertices[(mesh.Triangles[3*i]-1)*2+1];
+	s2x = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2];
+	s2y = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2+1];
+	s3x = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2];
+	s3y = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2+1];
+	//Propriétés du cercle circonscrit au triangle
+	double a, b, c, d, e, f, xcentre, ycentre, rayon
+	//Distance entre le point et le centre du cercle circonscrit
+	double dist=0.0;
+	//Admissible -1 si ne respecte pas le critère de Delaunay et 0 sinon
+	int Delaunay=0;
+
+	a = s2x - s1x;
+	b = s2y - s1y;
+	c = s3x - s1x;
+	d = s3y - s1y;
+	e = s2x*s2x - s1x*s1x + s2y*s2x - s1y*s1y;
+	f = s3x*s3x - s1x*s1x + s3y*s3y - s1y*s1y;
+	g = -(1./(2.*(a*d-b*c));
+
+	xcentre = g*( d*e - b*f);
+	ycentre = g*(-c*e + a*f);
+
+	rayon = distance2D(xcentre,ycentre,s1x,s1y);
+
+	dist  = distance2D(xcentre,ycentre,x,y);
+
+	if (dist<rayon)
+	{
+		Delaunay=-1;
+	}
+
+	return Delaunay;
 }
