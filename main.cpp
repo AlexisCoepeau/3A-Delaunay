3b8619c5379dd033ef48c5efba14d0c24020b1f8
GuillaumeDamour1 <guillaumedocitedamour@gmail.com>
2022-12-02 12:49:08 +0100

    ajout de cercle et corrections

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ struct maillage
 
 double distances2D(const double, const double, const double, const double) ;
 double Qualite2D(const int, const struct maillage) ;
+int admissibilite(double x, double y, int i, struct maillage mesh) ;
+void cercle(double *x, double *y, int N, int i, struct maillage mesh) ;
 
 
 /* Fonction principale
@@ -52,7 +54,6 @@ int main(int argc, char* argv[])
 	struct maillage mesh;
 	string ligne;
 	int N, indice;
-
 
 	if(monFlux)
 	{
@@ -121,20 +122,35 @@ int main(int argc, char* argv[])
 		}
 		monFlux.close();
 
+			cout << "yo" << endl;
 		//Affichage de la qualité
 		for(int i=0; i<2; i++)
 		{
 			cout << "-------------------------------------------------------" << endl;
 			cout << "Qualité triangle " << i << "=" << Qualite2D(i, mesh) << endl;
 		}
+			cout << "yo" << endl;
 	}
 
 	//On est sur le triangle contenant le point initial donc il est forcement noté -1
-	for (arretes du triangle contenant le point noté -1)
-		Tant qu'on ne tombe pas sur un triangle ayant deux voisins noté 0 on continue à aller sur ses triangles voisins
-			fonction(sur nouveau triangle)
-			Tant qu'on ne tombe pas sur un triangle ayant deux voisins noté 0 on continue à aller sur ses triangles voisins
-				fonction(sur nouveau triangle)
+	// for (arretes du triangle contenant le point noté -1)
+	// 	Tant qu'on ne tombe pas sur un triangle ayant deux voisins noté 0 on continue à aller sur ses triangles voisins
+	// 		fonction(sur nouveau triangle)
+	// 		Tant qu'on ne tombe pas sur un triangle ayant deux voisins noté 0 on continue à aller sur ses triangles voisins
+	// 			fonction(sur nouveau triangle)
+
+	//Test cercle et Delaunay
+	double *x, *y;
+	cercle(x, y, 100, 4, mesh);
+
+	ofstream monFlux2("cercle.dat");
+	for (int i=0; i<=N; i++)
+	{
+		monFlux2 << x[i] << y[i] << endl;
+	}
+
+
+
 
 
 	return 0;
@@ -172,15 +188,22 @@ double Qualite2D(const int i, const struct maillage mesh)
 int admissibilite(double x, double y, int i, struct maillage mesh)
 {
 	//Sommets du triangle i
-	double s1x, s1y, s2x, s2y;
-	s1x = mesh.Vertices[(mesh.Triangles[3*i]-1)*2];
-	s1y = mesh.Vertices[(mesh.Triangles[3*i]-1)*2+1];
-	s2x = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2];
-	s2y = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2+1];
-	s3x = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2];
-	s3y = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2+1];
+	double s1x, s1y, s2x, s2y, s3x, s3y;
+	// s1x = mesh.Vertices[(mesh.Triangles[3*i]-1)*2];
+	// s1y = mesh.Vertices[(mesh.Triangles[3*i]-1)*2+1];
+	// s2x = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2];
+	// s2y = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2+1];
+	// s3x = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2];
+	// s3y = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2+1];
+	s1x = 3.0;
+	s1y = 2.8;
+	s2x = 3.72;
+	s2y = 2.76;
+	s3x = 0.145;
+	s3y = 4.314;
+
 	//Propriétés du cercle circonscrit au triangle
-	double a, b, c, d, e, f, xcentre, ycentre, rayon
+	double a, b, c, d, e, f, g, xcentre, ycentre, rayon;
 	//Distance entre le point et le centre du cercle circonscrit
 	double dist=0.0;
 	//Admissible -1 si ne respecte pas le critère de Delaunay et 0 sinon
@@ -192,7 +215,7 @@ int admissibilite(double x, double y, int i, struct maillage mesh)
 	d = s3y - s1y;
 	e = s2x*s2x - s1x*s1x + s2y*s2x - s1y*s1y;
 	f = s3x*s3x - s1x*s1x + s3y*s3y - s1y*s1y;
-	g = -(1./(2.*(a*d-b*c));
+	g = -(1./(2.*(a*d-b*c)));
 
 	xcentre = g*( d*e - b*f);
 	ycentre = g*(-c*e + a*f);
@@ -207,4 +230,49 @@ int admissibilite(double x, double y, int i, struct maillage mesh)
 	}
 
 	return Delaunay;
+}
+
+void cercle(double *x, double *y, int N, int i, struct maillage mesh)
+{
+	//Sommets du triangle i
+	double s1x, s1y, s2x, s2y, s3x, s3y;
+	// s1x = mesh.Vertices[(mesh.Triangles[3*i]-1)*2];
+	// s1y = mesh.Vertices[(mesh.Triangles[3*i]-1)*2+1];
+	// s2x = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2];
+	// s2y = mesh.Vertices[(mesh.Triangles[3*i+1]-1)*2+1];
+	// s3x = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2];
+	// s3y = mesh.Vertices[(mesh.Triangles[3*i+2]-1)*2+1];
+	s1x = 3.0;
+	s1y = 2.8;
+	s2x = 3.72;
+	s2y = 2.76;
+	s3x = 0.145;
+	s3y = 4.314;
+
+	//Propriétés du cercle circonscrit au triangle
+	double a, b, c, d, e, f, g, xcentre, ycentre, rayon;
+	//Distance entre le point et le centre du cercle circonscrit
+	double dist=0.0;
+	//Admissible -1 si ne respecte pas le critère de Delaunay et 0 sinon
+	int Delaunay=0;
+	double theta;
+
+	a = s2x - s1x;
+	b = s2y - s1y;
+	c = s3x - s1x;
+	d = s3y - s1y;
+	e = s2x*s2x - s1x*s1x + s2y*s2x - s1y*s1y;
+	f = s3x*s3x - s1x*s1x + s3y*s3y - s1y*s1y;
+	g = -(1./(2.*(a*d-b*c)));
+
+	xcentre = g*( d*e - b*f);
+	ycentre = g*(-c*e + a*f);
+
+	rayon = distance2D(xcentre,ycentre,s1x,s1y);
+	theta = 2*(4*atan(1))/N;
+	for (int i=0; i<=N; i++)
+	{
+		x[i]=xcentre + rayon*cos(i*theta);
+		y[i]=ycentre + rayon*sin(i*theta);
+	}
 }
